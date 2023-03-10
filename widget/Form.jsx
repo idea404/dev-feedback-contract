e3910de4f107b8b0a083f6a39dd82f5407e84922
Dennis <10233439+idea404@users.noreply.github.com>
2023-03-10 13:47:58 +0100

    Widget button functions

--- widget/Form.jsx
+++ widget/Form.jsx
@@ -1,11 +1,34 @@
-const contract = "dev-feedback.idea404.near";
+const contract = "dev.feedback.idea404.testnet";
+
+State.init({ titleString: "", feedbackString: "" });
+
+const changeText = ({ target }) => {
+  State.update({ feedbackString: target.value });
+};
+
+const changeTitle = ({ target }) => {
+  State.update({ titleString: target.value });
+};
+
+const onBtnClick = () => {
+  if (!state.new_greeting) {
+    return;
+  }
+
+  Near.call(contract, "add_new_feedback", {
+    title: state.titleString,
+    text: state.new_greeting,
+  });
+};
 
 // Define components
 const greetingForm = (
   <>
     <div class="border border-black p-3">
+      <label>Give your feedback a title</label>
+      <input placeholder="Give your feedback a title here" onChange={changeTitle} />
       <label>Add feedback</label>
-      <input placeholder="Type your ideas here" onChange={onInputChange} />
+      <input placeholder="Type your ideas here" onChange={changeText} />
       <button class="btn btn-primary mt-2" onClick={onBtnClick}>
         Save
       </button>
